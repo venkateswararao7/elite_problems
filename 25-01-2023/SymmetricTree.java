c2d2d98c942e7a5afa37723c5a1d7c542def3a31
venkateswararao7 <85663469+venkateswararao7@users.noreply.github.com>
2023-01-25 19:56:19 +0530

    symmetric-tree

---
+++ 25-01-2023/SymmetricTree.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isMirror(TreeNode node1,TreeNode node2){
+        if(node1==null && node2==null)
+            return true;
+        if(node1!=null && node2!=null && node1.val==node2.val) 
+         return isMirror(node1.left,node2.right) && isMirror(node1.right,node2.left);
+        return false;
+    }
+    public boolean isSymmetric(TreeNode root) {
+        return isMirror(root,root);
+    }
+}
